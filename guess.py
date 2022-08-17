8bf33ad20fd1a6b8b09034a8367622fdbb825deb
karis courey <kariscourey@gmail.com>
2022-08-17 15:02:42 -0400

    Add guess.py

--- guess.py
+++ guess.py
@@ -2,25 +2,30 @@
 # Can use conditionals to define input
 input("Think of a number between 1 and 50. Press ENTER to start.")
 
-# Let computer know what we're guessing
+# Define guesses
+num_guesses = input("Hor many guesses does the computer get? ")
+num_guesses = int(num_guesses)
 
 # Define range of guess (min, max)
 low = 1
 high = 50
 
-# Guess integer between min, max
-guess = int((low + high) / 2)
-print("Is", guess, "your number? Please answer with higher, lower, or correct.")
+# 6 parts to a for loop: for, variable, in, collection, colon, body
+for guess_num in range(num_guesses): # range returns a collention ("sequence"), not an int; guess_num (variable) holds whatever gets returned from range()
 
-# Obtain and report answer
-answer = input("Answer: ").lower()
+    # Guess integer between min, max
+    guess = int((low + high) / 2)
+    print("Is", guess, "your number? Please answer with higher, lower, or correct.")
 
-# Tell if correct or incorrect
-if answer == "higher":
-    low = guess + 1
-elif answer == "lower":
-    high = guess - 1
-elif answer == "correct":
-    print("Computer correctly guessed:", guess) # input/output don't always have new line, may want to add \n
-else:
-    print("Invalid input! Try again.")
+    # Obtain and report answer
+    answer = input("Answer: ").lower()
+
+# # Tell if correct or incorrect
+# if answer == "higher":
+#     low = guess + 1
+# elif answer == "lower":
+#     high = guess - 1
+# elif answer == "correct":
+#     print("Computer correctly guessed:", guess) # input/output don't always have new line, may want to add \n
+# else:
+#     print("Invalid input! Try again.")
